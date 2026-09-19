87d23b40bcc282bfb96f8cf9bd8ba8a7d17db207
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-19 23:33:43 +0700

    chore: add Indonesia service barrel export

---
+++ src/services/indonesia/index.js
@@ -0,0 +1,13 @@
+export {
+ getIndonesiaNews,
+ getStockQuote,
+ getFuelPrices,
+ getElectricityPrices,
+ getFoodPrices,
+ findCity,
+ getPrayerSchedule,
+ upcomingRamadan,
+ refreshIndonesiaCache,
+ NEWS_CATEGORIES,
+ DATA_SOURCES
+} from './data.js';
