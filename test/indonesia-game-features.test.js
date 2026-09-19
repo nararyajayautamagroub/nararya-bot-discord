7bd62dbadcac488f776a82ecad753f5c4cfb2110
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-20 02:26:09 +0700

    test: align assertions with the canonical 150-feature registry

--- test/indonesia-game-features.test.js
+++ test/indonesia-game-features.test.js
@@ -28,7 +28,7 @@ test('Street View is not registered as a JKT48 mode',()=>{
 });
 
 test('feature registry contains the current implemented catalog',()=>{
- assert.equal(FEATURE_REGISTRY.length,151);
+ assert.equal(FEATURE_REGISTRY.length,150);
 });
 
 test('electronics scraper exposes public category sources',()=>{
