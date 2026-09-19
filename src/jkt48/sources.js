f831c15df9977afe03b6f1ca8c35026e1c250c21
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-19 22:25:01 +0700

    feat: add modular JKT48 feed integration

---
+++ src/jkt48/sources.js
@@ -0,0 +1,15 @@
+export const DEFAULT_SOURCES=[
+ ['jkt48-web','JKT48 Website','jkt48-web','https://jkt48.com/'],
+ ['jkt48-events','JKT48 Events','jkt48-web','https://jkt48.com/events'],
+ ['jkt48-news','JKT48 News','jkt48-web','https://jkt48.com/news'],
+ ['jkt48-theater','JKT48 Theater','jkt48-web','https://jkt48.com/theater'],
+ ['idn-jkt48','IDN JKT48 Live','idn','https://www.idn.app/'],
+ ['showroom-jkt48','SHOWROOM JKT48','showroom','https://www.showroom-live.com/'],
+ ['youtube-jkt48','YouTube JKT48','youtube','https://www.youtube.com/@JKT48'],
+ ['instagram-jkt48','Instagram JKT48','instagram','https://www.instagram.com/jkt48/'],
+ ['tiktok-jkt48','TikTok JKT48','tiktok','https://www.tiktok.com/@jkt48'],
+ ['x-jkt48','X/Twitter JKT48','x','https://x.com/officialJKT48'],
+ ['threads-jkt48','Threads JKT48','threads','https://www.threads.net/'],
+ ['tokopedia-jkt48','Tokopedia JKT48','tokopedia','https://www.tokopedia.com/'],
+ ['shopee-jkt48','Shopee JKT48','shopee','https://shopee.co.id/']
+];
