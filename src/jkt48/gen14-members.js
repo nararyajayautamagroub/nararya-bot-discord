6ee3eafad811ebae327932378917529ff9090483
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-20 02:41:09 +0700

    feat: add JKT48 generation 14 seed data

---
+++ src/jkt48/gen14-members.js
@@ -0,0 +1,14 @@
+export const JKT48_GEN14_MEMBERS = Object.freeze([
+ {name:'Afera Thalia',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Fera',idn_url:'https://idn.onelink.me/VKUf/FeraJKT48'},
+ {name:'Carissa Dini',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Carissa',idn_url:'https://idn.onelink.me/VKUf/CarissaJKT48'},
+ {name:'Christabella Bonita',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Bella',idn_url:'https://idn.onelink.me/VKUf/BellaJKT48'},
+ {name:'Fahira Putri',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Fahira',idn_url:'https://idn.onelink.me/VKUf/FahiraJKT48'},
+ {name:'Fatimah Azzahra',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Rara',idn_url:'https://idn.onelink.me/VKUf/RaraJKT48'},
+ {name:'Heidi Suyangga',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Heidi',idn_url:'https://idn.onelink.me/VKUf/HeidiJKT48',instagram_url:'https://www.instagram.com/heidi.jkt48/',tiktok_url:'https://www.tiktok.com/@jkt48.heidi__',x_url:'https://x.com/Heidi_JKT48'},
+ {name:'Maxine Faye Lee',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Maxine',idn_url:'https://idn.onelink.me/VKUf/MaxineJKT48'},
+ {name:'Putry Jazyta',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Jazzy',idn_url:'https://idn.onelink.me/VKUf/JazzyJKT48'},
+ {name:'Ralyne Van Irwan',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Ralyne',idn_url:'https://idn.onelink.me/VKUf/RalyneJKT48'},
+ {name:'Sona Kalyana',generation:14,status:'trainee',showroom_url:'https://www.showroom-live.com/r/JKT48_Sona',idn_url:'https://idn.onelink.me/VKUf/SonaJKT48'}
+]);
+
+export const GEN14_SOURCE_URL='https://jkt48.com/news/pengumuman-mengenai-akun-showroom-idn-live-member-generasi-14-jkt48';
