21d5b9a1587acb1fd25d545101f474031304f187
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-19 22:44:56 +0700

    data: add JKT48V member records and official cohort model

---
+++ src/jkt48/jkt48v-members.js
@@ -0,0 +1,10 @@
+export const JKT48V_MEMBERS=[
+ {id:'pia-meraleo',name:'Pia Meraleo',cohort:'original',status:'graduated',youtube_url:'https://www.youtube.com/@piameraleo-jkt48v',x_url:'https://x.com/pia_jkt48v',instagram_url:'https://www.instagram.com/jkt48v.pia/',tiktok_url:'https://www.tiktok.com/@jkt48v.pia',graduation_date:'2026-09-05'},
+ {id:'tana-nona',name:'Tana Nona',cohort:'original',status:'graduated',youtube_url:'https://www.youtube.com/@tananona-jkt48v',x_url:'https://x.com/tana_jkt48v',instagram_url:'https://www.instagram.com/jkt48v.tana/',tiktok_url:'https://www.tiktok.com/@jkt48v.tana',graduation_date:'2026-07-27'},
+ {id:'kanaia-asa',name:'Kanaia Asa',cohort:'original',status:'historical',youtube_url:'https://www.youtube.com/@kanaiaasa-jkt48v',x_url:'https://x.com/kanaiaasa-jkt48v',instagram_url:'https://www.instagram.com/jkt48v.kanaia/',tiktok_url:'https://www.tiktok.com/@jkt48v.kanaia'},
+ {id:'maura-nilambari',name:'Maura Nilambari',cohort:'later',status:'graduated',graduation_date:'2026-08-09'},
+ {id:'isha-kirana',name:'Isha Kirana',cohort:'later',status:'graduated',graduation_date:'2026-08-21'},
+ {id:'sami-maono',name:'Sami Maono',cohort:'later',status:'ended',graduation_date:null}
+];
+// JKT48 officially presented JKT48V as a virtual sub-unit, not as numbered generations.
+// "original"/"later" is used here instead of inventing Gen 1/2/3 labels.
