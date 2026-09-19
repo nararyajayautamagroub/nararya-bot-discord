537717c001b8897706e7bce53356b4020bf2b3d3
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-19 21:47:24 +0700

    feat: add core JKT48 notification and Discord automation

---
+++ src/deploy-commands.js
@@ -0,0 +1,1 @@
+import 'dotenv/config'; import {REST,Routes} from 'discord.js'; import {commandData} from './index.js'; const rest=new REST({version:'10'}).setToken(process.env.DISCORD_TOKEN); await rest.put(Routes.applicationCommands(process.env.CLIENT_ID),{body:commandData.map(x=>x.toJSON())}); console.log('Commands deployed');
