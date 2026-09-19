406fa20929394a9c14cfc9149b868ce1958a8e42
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-19 23:51:07 +0700

    refactor: remove generic Street View mode from JKT48 modes

--- src/services/games/jkt48/index.js
+++ src/services/games/jkt48/index.js
@@ -5,8 +5,7 @@ export const MODES={
  graduationPhoto:'Tebak member dari foto graduation',
  randomMember:'Tebak member random',
  setlistPhoto:'Tebak foto setlist',
- songPhoto:'Tebak lagu dari foto',
- streetView:'Tebak lokasi dari Google Street View'
+ songPhoto:'Tebak lagu dari foto'
 };
 export const RARITIES=['common','uncommon','rare','epic','legendary','mythic','secret'];
 export const WEIGHTS={common:.50,uncommon:.25,rare:.13,epic:.07,legendary:.035,mythic:.014,secret:.001};
